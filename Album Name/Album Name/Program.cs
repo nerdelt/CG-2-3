8a4ebedb82a532906bc715873fb6c21bb71fb610
nerdelt <nerdelt@gmail.com>
2018-07-18 12:14:18 -0500

    Assignment 3: Album name

---
+++ Album Name/Album Name/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Album_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Have a real or made-up band? Let's find the perfect name for your first album!");
+            Console.Write("What is your favorite ice cream flavor? ");
+            string myIceCream = Console.ReadLine();
+
+            Console.Write("What is your favorite animal? ");
+            string myAnimal = Console.ReadLine();
+
+            Console.Write("What is your favorite season? ");
+            string mySeason = Console.ReadLine();
+
+            Console.WriteLine("Your album name is: " + myIceCream + " " + myAnimal + "s " + "in the " + mySeason);
+            Console.ReadLine();
+
+
+        }
+    }
+}
